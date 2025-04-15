e24cd6258655eccd80d0ebb1c859f592c98353a9
Manav Goel <mgoel283@gmail.com>
2025-04-15 14:15:56 -0400

    slim gpxchunk tag

--- markdoc/tags/gpxchunk.markdoc.ts
+++ markdoc/tags/gpxchunk.markdoc.ts
@@ -1,11 +1,5 @@
-import {GPXChunk} from '../../components';
+import { GPXChunk } from '../../components';
 
 export const gpxchunk = {
   render: GPXChunk,
-  children: ['paragraph', 'tag', 'list'],
-  attributes: {
-    title: {
-      type: String,
-    },
-  },
 };
