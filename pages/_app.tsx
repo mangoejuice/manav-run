b2370023eefd12f3881f5f0ef9432ef165ab0c0e
Manav Goel <mgoel283@gmail.com>
2025-10-23 21:46:39 -0400

    safe-area-inset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -84,7 +84,7 @@ export default function MyApp({ Component, pageProps }: AppProps<MyAppProps>) {
     <>
       <Head>
         <title>{title}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
         <meta name="referrer" content="strict-origin" />
         <meta name="title" content={title} />
         <meta name="description" content={description} />
